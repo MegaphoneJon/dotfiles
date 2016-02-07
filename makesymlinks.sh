ab8a99434a8aadd2171b5057462dd10f7093dece
Jon goldberg <jon@palantetech.coop>
2016-02-07 17:53:37 -0500

    Install oh-my-zsh when installing dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim ptrrc gitconfig profile selected_editor zshenv"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim ptrrc gitconfig profile selected_editor zshenv oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -29,3 +29,5 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
